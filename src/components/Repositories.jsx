086111866e442b64c783d2d372c4e405b713e2f1
geletatamiru <150704342+geletatamiru@users.noreply.github.com>
2025-05-11 12:38:08 +0300

    Update Repositories.jsx
    fixed the issue to show their names

--- src/components/Repositories.jsx
+++ src/components/Repositories.jsx
@@ -4,7 +4,7 @@ import "./Repositories.css";
 const Repositories = ({repos,count}) => {
   return (
     <div className="repositories">
-      <h2>Bekacru's Public repositories ({count})</h2>
+      <h2>{repos.name || repos.login} Public repositories ({count}})</h2>
       {
         repos.map((repo) => {
           return <RepoCard key={repo.id} repo={repo} />
